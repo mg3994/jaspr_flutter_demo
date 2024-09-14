d65692d330ebf937bca24e15cc9edc3d8a052d8c
mg3994 <manishgautammg7@yahoo.com>
2024-09-14 23:28:53 +0530

    first commit

--- lib/jaspr_options.dart
+++ lib/jaspr_options.dart
@@ -2,6 +2,11 @@
 // Generated with jaspr_builder
 
 import 'package:jaspr/jaspr.dart';
+import 'package:antinna/components/counter.dart' as prefix0;
+import 'package:antinna/components/header.dart' as prefix1;
+import 'package:antinna/pages/about.dart' as prefix2;
+import 'package:antinna/pages/home.dart' as prefix3;
+import 'package:antinna/app.dart' as prefix4;
 
 /// Default [JasprOptions] for use with your jaspr project.
 ///
@@ -19,4 +24,15 @@ import 'package:jaspr/jaspr.dart';
 ///   runApp(...);
 /// }
 /// ```
-final defaultJasprOptions = JasprOptions();
+final defaultJasprOptions = JasprOptions(
+  clients: {
+    prefix2.About: ClientTarget<prefix2.About>('pages/about'),
+    prefix3.Home: ClientTarget<prefix3.Home>('pages/home'),
+  },
+  styles: () => [
+    ...prefix0.CounterState.styles,
+    ...prefix1.Header.styles,
+    ...prefix2.About.styles,
+    ...prefix4.App.styles,
+  ],
+);
